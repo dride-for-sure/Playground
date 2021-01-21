597bc0fb4d1ccf900fc442e87186d85be84a31e4
dennisjauernig <dennis@jauernig.email>
2021-01-21 16:28:13 +0100

    Switch expression

--- src/main/java/Thursday.java
+++ src/main/java/Thursday.java
@@ -8,15 +8,12 @@ public class Thursday {
  }
 
  public static boolean alarmHandler (int number, String alarmStatus) {
-	switch ( alarmStatus ) {
-	 case "red":
-		return false;
-	 case "yellow":
-		return number <= 30;
-	 case "green":
-		return number <= 60;
-	}
-	return true;
+	return switch ( alarmStatus ) {
+	 case "red" -> false;
+	 case "yellow" -> number <= 30;
+	 case "green" -> number <= 60;
+	 default -> true;
+	};
  }
 
  public static int factorial (int number) {
