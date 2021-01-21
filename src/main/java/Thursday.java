1243202075fa3f2cb8391a9da5b4ed8db8590bc5
dennisjauernig <dennis@jauernig.email>
2021-01-21 15:44:43 +0100

    Factorial via recursion

--- src/main/java/Thursday.java
+++ src/main/java/Thursday.java
@@ -18,4 +18,12 @@ public class Thursday {
 	}
 	return true;
  }
+
+ public static int factorial (int number) {
+	if ( number == 0 ) {
+	 return 1;
+	} else {
+	 return ( number * factorial( number - 1 ) );
+	}
+ }
 }
--- src/test/java/ThursdayTests.java
+++ src/test/java/ThursdayTests.java
@@ -1,13 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
-import java.util.zip.CheckedInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,6 +35,22 @@ public class ThursdayTests {
 					Arguments.of( 60, "green", true ),
 					Arguments.of( 61, "green", false ),
 					Arguments.of( 1, "red", false )
+	);
+ }
+
+ // Method Source
+ @DisplayName ("Check factorial")
+ @ParameterizedTest (name = "{0}! is {1}")
+ @MethodSource
+ void factorialTest (int number, int expected) {
+	assertEquals( expected, Thursday.factorial( number ) );
+ }
+
+ private static Stream<Arguments> factorialTest () {
+	return Stream.of(
+					Arguments.of( 0, 1 ),
+					Arguments.of( 1, 1 ),
+					Arguments.of( 5, 120 )
 	);
  }
 }
